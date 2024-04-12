3f86aa09a4fd262875c867c10c050f7aaf07f0dc
qnbby <quan.nguyen.duc.3010@gmail.com>
2024-04-12 16:12:14 -0400

    stupid darren

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -3,6 +3,8 @@ import 'package:recipe_cart/common/ui/homepage/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:recipe_cart/common/ui/homepage/widgets/camera_screen.dart';
+import 'package:recipe_cart/common/ui/homepage/widgets/preference_page.dart';
+import 'package:recipe_cart/common/ui/homepage/widgets/avoidance_page.dart';
 
 final router = GoRouter(
   routes: [
